d8fcc605d757a964e13b5366be591a10c8e88162
Suyashcoder117 <75789654+Suyashcoder117@users.noreply.github.com>
2021-05-16 10:16:11 +0530

    Update fwitter_room.js

--- fwitter_room.js
+++ fwitter_room.js
@@ -50,5 +50,5 @@ function redirectToRoomName(name)
 function logout() {
 localStorage.removeItem("user_name");
 localStorage.removeItem("room_name");
-    window.location = "fwitter.html";
+    window.location = "index.html";
 }
